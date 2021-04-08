18f7fd6f01a2027b5fb5bb9ab505304c527c60a7
Zuzanna Grzegorzewska <zuzannagrzeg@wp.pl>
2021-04-08 11:18:56 +0200

    Add javascript code - constructor method - announce/initActions

--- src/js/script.js
+++ src/js/script.js
@@ -307,7 +307,9 @@
 
     announce () {
       const thisWidget = this;
-      const event = new Event ('updated');
+      const event = new CustomEvent ('updated', {
+        bubbles: true
+      });
 
       thisWidget.element.dispatchEvent (event);
     };
@@ -352,6 +354,10 @@
       thisCart.dom.toggleTrigger.addEventListener ('click', function (Event) {
         Event.preventDefault();
         thisCart.dom.wrapper.classList.toggle (classNames.cart.wrapperActive);
+      });
+
+      thisCart.dom.productList.addEventListener ('updated', function() {
+        thisCart.update();
       });
     };
 
